d77ba9f85fc09bca5a6f4ccdd74b7d28641b6904
xpinked <azsd91@gmail.com>
2017-04-23 22:10:59 +0300

    Help ME!

--- py.py
+++ py.py
@@ -6,51 +6,45 @@ import random
 import string
 from random import randint
 
+#Inventory Checking
+def check_inventory():
+    global inventory
+    damage = ''
+    if 'sword' in inventory:
+        damage = randint(5,25)
+    elif 'wand' in inventory:
+        damage = randint(10,32)
+    elif 'gun' in inventory:
+        damage = randint(20,45)
+    return damage
+
 #All user information and stats
 class Character:
     def __init__(self):
         self.username = ''
         self.health = 1
         self.health_max = 1
+        self.ename = ''
 
-    def do_damage(self, enemies):
-        damage = min(max(randint(0, self.health) - randint(0, enemies.health), 0),enemies.health)
-        enemies.health = enemies.health - damage
-        if damage == 0:
-            print "The {} evaded the attack., try again, better this time.. k?".format(enemies.name)
+    def do_damage(self, Enemies):
+        c_invent = check_inventory()
+        Enemies.health = Enemies.health - c_invent
+        if c_invent > 0:
+            print "Take that!! **& {} You bastard ".format(char.ename)
+            time.sleep(2)
+            print "{} been hitted {}".format(char.ename, c_invent)
+            print " Enemies health is {}".format(Enemies.health)
         else:
-            print "Take that!! **&^"
-            print " Enemies health is {}".format(enemies.health)
-        return enemies.health <= 0
+            print "The {} evaded the attack., try again, better this time.. k?".format(char.ename)
+        return Enemies.health
 
 
 class Enemies(Character):
-    ##Enemies:
-    e_names = [
-    'Goblin','Slime',
-    'Barbarian', 'Cocroach',
-    'Dulalhan']
-
-    def __init__(self, player):
-        Character.__init__()
-        self.name = self.stats(self.e_names[randint(0,4)])
+    def __init__(self):
+        Character.__init__(self)
         self.health = ''
 
-    def stats(self, name):
-        if self.name == self.e_names[0]:
-            self.health = 60
-        elif self.name == self.e_names[1]:
-            self.health = 80
-        elif name == self.e_names[2]:
-            self.health = 100
-        elif name == self.e_names[3]:
-            self.health = 120
-        elif name == self.e_names[4]:
-            self.health = 140
-        return self.health
-
-
-class Player(Character):
+class Player(Character, Enemies):
     def __init__(self):
         Character.__init__(self)
         self.state = 'refreshed'
@@ -58,6 +52,9 @@ class Player(Character):
         self.health_max = 100
 
 #Commands Functions
+    def my_inventory(self):
+        print inventory
+
     def leavegame(self):
         print " %s Baka.. Why are you leaving? hard huh...?" % self.username
         self.health = 0
@@ -66,7 +63,13 @@ class Player(Character):
         print Commands.keys()
 
     def status(self):
-        print "%s's health: %d/%d" % (self.username, self.health, self.health_max)
+        if Enemies.health > 0:
+            print "%s's health: %d/%d" % (self.username, self.health, self.health_max)
+            print "{} health: {}".format(self.ename, Enemies.health)
+            print "%s's state: %d/%d" % (self.username, self.state)
+        else:
+            print "%s's health: %d/%d" % (self.username, self.health, self.health_max)
+            print "%s's state: %d/%d" % (self.username, self.state)
 
     def tired(self):
         print "%s You're feeling tired, you should rest.." % self.username
@@ -75,16 +78,15 @@ class Player(Character):
     def rest(self):
         if self.state != 'refreshed':
             print "%s can't rest now!" % self.username
-            self.enemies_attacks()
+            self.Enemies_attacks()
         else:
             print "%s Drinks a coke, and relaxes.." % self.username
             print "It is so good to rest, num num, cookies, num num!"
             time.sleep(4)
         if randint(0, 1):
-            self.enemies = Enemies(self)
-            print "You're been awaken by that stupid %s!" % (self.enemies.name)
+            print "You're been awaken by that stupid {}!".format(self.ename)
             self.state = 'fight'
-            self.enemies_attacks()
+            self.Enemies_attacks()
         else:
             if self.health < self.health_max:
                 self.health = self.health + 1
@@ -95,15 +97,14 @@ class Player(Character):
     def explore(self):
         if self.state != 'refreshed':
             print "%s Common... you're in a middle of a fight!! attack or run! &^#@!*" % self.username
-            self.enemies_attacks()
+            self.Enemies_attacks()
         else:
             print "%s explores a twisty passage." % self.username
-            if randint(0, 1):
-                self.enemies = Enemies(self)
-                print "%s encounters %s!" % (self.username, self.enemies.name)
+            if randint(0, 3):
+                print "%s encounters %s!" % (self.username, self.ename)
                 self.state = 'fight'
             else:
-                if randint(0, 1):
+                if randint(0, 6):
                     self.tired()
 
     def flee(self):
@@ -111,33 +112,33 @@ class Player(Character):
             print "%s runs in circles for a while." % self.username
             self.tired()
         else:
-            if randint(1, self.health + 5) > randint(1, self.enemies.health):
-                print "%s flees from %s." % (self.username, self.enemies.name)
-                self.enemies = None
+            if randint(1, self.health + 5) > randint(1, Enemies.health):
+                print "%s flees from %s." % (self.username, self.ename)
                 self.state = 'refreshed'
             else:
-                print "%s couldn't escape from %s!" % (self.username, self.enemies.name)
-                self.enemies_attacks()
+                print "%s couldn't escape from %s!" % (self.username, self.ename)
+                self.Enemies_attacks()
 
     def attack(self):
         if self.state != 'fight':
             print "%s swats the air, without notable results." % self.username
             self.tired()
         else:
-            if self.do_damage(self.enemies):
-                print "{} executes {}!".format(self.username, self.enemies.name)
-                self.enemies = None
+            if self.do_damage(Enemies):
+                print "{} executes {}!".format(self.username, self.ename)
                 self.state = 'refreshed'
                 if randint(0, self.health) < 10:
                     self.health = self.health + 1
                     self.health_max = self.health_max + 1
                     print "%s feels stronger!" % self.username
                 else:
-                    self.enemies_attacks()
+                    self.Enemies_attacks()
 
-    def enemies_attacks(self):
-         if self.do_damage(self):
-             print "Ohh boy... The %s won, he killed you!!!!" % (self.enemies.name)
+    def Enemies_attacks(self):
+         if self.do_damage(self)  <= 0:
+             print "Ohh boy... The %s won, he killed you!!!!" % (self.ename)
+
+##Functions for later use
 
 #generate password for later user
 def generatepassword():
@@ -148,11 +149,37 @@ def generatepassword():
 
 
 #All important variables
+##Enemies names:
+e_names = ['Goblin','Slime','Barbarian', 'Cocroach','Dulalhan']
 coins=1
 gen_password = generatepassword()
-inventory =[
+inventory =['sword']
+#Player Commands
+Commands = {
+  'leavegame': Player.leavegame,
+  'commands': Player.help,
+  'status': Player.status,
+  'rest': Player.rest,
+  'explore': Player.explore,
+  'run': Player.flee,
+  'stab': Player.attack,
+  'inventory': Player.my_inventory
+  }
 
-]
+#defines Enemies names
+def stats(name):
+    if name == e_names[0]:
+        Enemies.health = 60
+    elif name == e_names[1]:
+        Enemies.health = 80
+    elif name == e_names[2]:
+        Enemies.health = 100
+    elif name == e_names[3]:
+        Enemies.health = 120
+    elif name == e_names[4]:
+        Enemies.health = 140
+    return name
+
 
 #Coin adding system
 #loop = trollius.get_event_loop()
@@ -164,36 +191,23 @@ def coinadd():
           return coins
     #loop.stop()
 
-#Player Commands
-Commands = {
-  'leavegame': Player.leavegame,
-  'commands': Player.help,
-  'status': Player.status,
-  'rest': Player.rest,
-  'explore': Player.explore,
-  'run': Player.flee,
-  'stab': Player.attack,
-  }
-
-
-
 def journey():
-    while user.health > 0 :
+    while char.health > 0 :
         line = raw_input("> ")
         args = line.split()
         if len(args) > 0:
             commandFound = False
             for c in Commands.keys():
                 if args[0] == c[:len(args[0])]:
-                    Commands[c](user)
+                    Commands[c](char)
                     commandFound = True
                     break
             if not commandFound:
-                print "%s doesn't understand the suggestion." % user.username
+                print "%s doesn't understand the suggestion." % char.username
 
 
 def game():
-    print "Welcome",user.username,"Welcome to buy and sell game"
+    print "Welcome",char.username,"Welcome to buy and sell game"
     print "Currently you have",coins,"coins"
     if coins == 0:
       print "Well Welcome then new player, the game is very easy"
@@ -206,11 +220,11 @@ def game():
             well lets start
             """
     else:
-      print "Ohh welcome back!",user.username,"want to continue you're play? >>"
+      print "Ohh welcome back!",char.username,"want to continue you're play? >>"
       choose = raw_input('')
       if choose.lower() == "yes":
           print "Horray!"
-          print "%s lets continue our journey shall we?" % (user.username)
+          print "%s lets continue our journey shall we?" % (char.username)
           #loop.call_soon(coinadd, loop)
           #loop.run_forever()
           #loop.close()
@@ -218,8 +232,8 @@ def game():
 
 def beforestart():
     global username
-    if user.username == "Udi":
-        print "Welcome", user.username, "You have logged in\n"
+    if char.username == "Udi":
+        print "Welcome", char.username, "You have logged in\n"
         time.sleep(2)
         print "Would you like to play a game? Yes/No: "
         press = raw_input("")
@@ -229,12 +243,13 @@ def beforestart():
         else:
             print "Ohh.. i see, see you next time!\n"
     else:
-        print "User",user.username,"is not registred.."
+        print "User",char.username,"is not registred.."
 
 if __name__ == '__main__':
-    user = Player()
-    user.username = (raw_input("Put here Your\'re name please: "))
-    print "Hey there",user.username,"Here is you're password: ",gen_password
+    char = Player()
+    char.username = (raw_input("Put here Your\'re name please: "))
+    char.ename = stats(e_names[randint(0,4)])
+    print "Hey there",char.username,"Here is you're password: ",gen_password
     print "Keep this password in hand"
     time.sleep(3)
     print "Very well..."
